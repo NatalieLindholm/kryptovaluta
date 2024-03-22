3a8d483f1cfd7b338b7d12c3ca76c5f37e222f90
Natalie Lindholm <natalie.lindholm05@gmail.com>
2024-03-22 10:05:06 +0200

    crypto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import Link from "next/link";
-import { FaArrowUp } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 export default async function Home() {
   return (
@@ -10,25 +20,47 @@ export default async function Home() {
           <h1>Currency</h1>
           <h1>App</h1>
         </div>
+        {/*
+        <Link href={"/trade"}>
+          <button>Trade</button>
+        </Link> */}
 
-        <div className="buttons">
-          <Link href={"/trade"}>
-            <button>Trade</button>
-          </Link>
-          {/* <Link href={"/cryptos"}>
-            <button>Cryptos</button>
-          </Link> */}
-        </div>
+        <Drawer>
+          <DrawerTrigger className="DrawerTrigger">Open</DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+              <DrawerDescription>
+                This action cannot be undone.
+              </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter>
+              <Button>Submit</Button>
+              <DrawerClose>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
       </div>
 
       <div>{/* Your bought crypto here */}</div>
       <div className="portfolio">
-        <h2>BTC</h2>
-        <p>Time Bought: 10:23</p>
-        <p>Units: 1</p>
-        <p>Price Bought: 232323</p>
-        <h3>Current Price: 4545</h3>
-        <FaArrowUp />
+        <h2>
+          <b>BTC</b>
+        </h2>
+        <p>
+          Time Bought: <b>10:23</b>
+        </p>
+        <p>
+          Units: <b>1</b>
+        </p>
+        <p>
+          Price Bought: <b>232323</b>
+        </p>
+        <h3>
+          Current Price: <b>4545</b>
+        </h3>
       </div>
     </div>
   );
